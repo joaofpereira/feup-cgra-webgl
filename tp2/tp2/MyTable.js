2700fb99de9dae6b33b99039ec610d1378a3d59e
JFPereira <pereiraffjoao1993@gmail.com>
2016-02-28 00:47:05 +0000

    starting tp2

---
+++ tp2/tp2/MyTable.js
@@ -0,0 +1,51 @@
+/**
+ * MyTable
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+function MyTable(scene) {
+	CGFobject.call(this,scene);
+
+	this.cube = new MyUnitCubeQuad(this.scene);
+};
+
+MyTable.prototype = Object.create(CGFobject.prototype);
+MyTable.prototype.constructor=MyTable;
+
+MyTable.prototype.display = function () {
+	// base
+	this.scene.pushMatrix();
+	this.scene.translate(0, (3.5/2) + (0.3/2), 0);
+    this.scene.scale(5, 0.3, 3);
+    this.cube.display();
+    this.scene.popMatrix();
+
+	//legs
+	// front right
+    this.scene.pushMatrix();
+    this.scene.translate((5/2) - 0.3, 0, (3/2) - 0.3);
+    this.scene.scale(0.3, 3.5, 0.3);
+    this.cube.display();
+    this.scene.popMatrix();
+
+    // front left
+    this.scene.pushMatrix();
+    this.scene.translate(((5/2) - 0.3) * (-1), 0, (3/2) - 0.3);
+    this.scene.scale(0.3, 3.5, 0.3);
+    this.cube.display();
+    this.scene.popMatrix();
+
+    // back left
+    this.scene.pushMatrix();
+    this.scene.translate(((5/2) - 0.3), 0, ((3/2) - 0.3) * (-1));
+    this.scene.scale(0.3, 3.5, 0.3);
+    this.cube.display();
+    this.scene.popMatrix();
+
+    // back right
+    this.scene.pushMatrix();
+    this.scene.translate(((5/2) - 0.3) * (-1), 0, ((3/2) - 0.3) * (-1));
+    this.scene.scale(0.3, 3.5, 0.3);
+    this.cube.display();
+    this.scene.popMatrix();
+};
